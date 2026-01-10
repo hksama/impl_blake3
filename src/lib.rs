f74011e92972ebfc1a98ec65d3d878d1511247bc
EvilGenius1010 <kolhatkarh@gmail.com>
2026-01-10 13:49:28 +0530

    brute-force pre-unit testing impl before merkle trees

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 type Word = u32;
 
-
-static iv:[u32;8] = [0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19];
-
+static iv: [u32; 8] = [
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+];
 
 #[derive(Debug)]
 struct DataChunks<'a> {
@@ -11,71 +11,56 @@ struct DataChunks<'a> {
 
     /// Input data sliced into 64*64 bits.
     /// First vector is for all chunks of 1024 bytes and
-    slices: (ReturnedChunks<'a>, &'a [u8]),
+    slices: (Vec<[u8; 1024]>, &'a [u8]),
 
     /// No of chunks input data broken into.
     chunks: usize,
 }
 
-union ReturnedChunks<'a> {
-    non_empty_chunks: &'a [[u8; 512]],
-    empty_chunks: [u8; 0],
-}
+/// breaks given data into chunks of 1024 bytes by mutating passed data.
+fn break_into_chunks_at_once_mut<'a>(input_data: &'a mut [u8]) -> DataChunks<'a> {
+    /*
+    let chunks = input_data.len().div_ceil(1024);
+    let mut mem_ptr_front: usize = 0;
+    let mut mem_ptr_back: usize = 0;
+    let mut chunks_vec: Vec<[u8; 512]> = Vec::new();
 
-pub enum BreakIntoChunksOptions {
-    /// Mutate passed input.
-    MutateValue(Vec<u8>),
-
-    /// Creates a copy of the values and works on them.
-    CopyValue(Vec<u8>),
-}
-
-fn break_into_chunks_at_once<'a>(input_data_with_flags: BreakIntoChunksOptions) -> DataChunks<'a> {
-    match input_data_with_flags {
-        BreakIntoChunksOptions::CopyValue(input_data) => {
-            let chunks = input_data.len().div_ceil(1024);
-            let mut mem_ptr_front: usize = 0;
-            let mut mem_ptr_back: usize = 0;
-            let mut chunks_vec: Vec<[u8; 512]> = Vec::new();
-
-            // input is less than 1024 elements
-            if (mem_ptr_front + 1024).min(input_data.len()) == input_data.len() {
-                return DataChunks {
-                    input_len: input_data.len(),
-                    slices: ([], input_data.clone().as_slice()),
-                    chunks: 1,
-                };
-            }
-
-            while mem_ptr_front <= input_data.len() && mem_ptr_back < input_data.len() {
-                // println!("{},{},{} \n",mem_ptr_front,mem_ptr_back,input_data[mem_ptr_back..mem_ptr_front].len());
-                mem_ptr_front = (mem_ptr_front + 1024).min(input_data.len());
-
-                chunks_vec.push(input_data[mem_ptr_back..mem_ptr_front].try_into().unwrap());
-                mem_ptr_back = (mem_ptr_back + 1024).min(input_data.len());
-                // println!("{},{} \n",mem_ptr_front,mem_ptr_back);
-            }
-            return DataChunks {
-                input_len: input_data.len(),
-                slices: chunks_vec.as_slice(),
-                chunks: chunks,
-            };
-        }
-        BreakIntoChunksOptions::MutateValue(input_data) => {
-            let (chunks, remainder) = input_data.as_chunks::<512>();
-
-            return DataChunks {
-                input_len: input_data.len(),
-                slices: (chunks, remainder),
-                chunks: chunks.len(),
-            };
-        }
+    input is less than 1024 elements
+    if (mem_ptr_front + 1024).min(input_data.len()) == input_data.len() {
+        return DataChunks {
+            input_len: input_data.len(),
+            slices: ([], input_data.clone().as_slice()),
+            chunks: 1,
+        };
     }
+
+    while mem_ptr_front <= input_data.len() && mem_ptr_back < input_data.len() {
+        // println!("{},{},{} \n",mem_ptr_front,mem_ptr_back,input_data[mem_ptr_back..mem_ptr_front].len());
+        mem_ptr_front = (mem_ptr_front + 1024).min(input_data.len());
+
+        chunks_vec.push(input_data[mem_ptr_back..mem_ptr_front].try_into().unwrap());
+        mem_ptr_back = (mem_ptr_back + 1024).min(input_data.len());
+        // println!("{},{} \n",mem_ptr_front,mem_ptr_back);
+    }
+    return DataChunks {
+        input_len: input_data.len(),
+        slices: chunks_vec.as_slice(),
+        chunks: chunks,
+    };
+    */
+
+    let (chunks, remainder) = input_data.as_chunks::<1024>();
+
+    return DataChunks {
+        input_len: input_data.len(),
+        slices: (chunks.to_vec(), remainder),
+        chunks: chunks.len(),
+    };
 }
 
 /// permute fn as  which shifts values at indices given in input_indices to shifted_indices
 /// Mentioned in https://www.ietf.org/archive/id/draft-aumasson-blake3-00.html#name-message-word-permutation
-fn permute(data_chunks: ()) {
+fn permute(data_chunks: [u32; 16]) {
     let input_indices: [usize; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     let shifted_indices: [usize; 16] = [2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8];
 }
@@ -88,34 +73,50 @@ fn quarter_round_fn(
     b: usize,
     c: usize,
     d: usize,
-    x: [Word; 1],
-    y: [Word; 1],
-)->[u32;16] {
-    word_in_process[a] = (word_in_process[a] + word_in_process[b] + x[0]) % u32::MAX;
-    word_in_process[d] = (word_in_process[d] ^ word_in_process[a]).rotate_right(16); 
+    x: Word,
+    y: Word,
+) -> [u32; 16] {
+    word_in_process[a] = (word_in_process[a] + word_in_process[b] + x) % u32::MAX;
+    word_in_process[d] = (word_in_process[d] ^ word_in_process[a]).rotate_right(16);
     word_in_process[c] = (word_in_process[c] + word_in_process[d]) % u32::MAX;
-    word_in_process[b] = (word_in_process[b] ^ word_in_process[c]).rotate_right(12); 
-    word_in_process[a] = (word_in_process[a] + word_in_process[b] + y[0]) % u32::MAX;
-    word_in_process[d] = (word_in_process[d] ^ word_in_process[a]).rotate_right(12); 
+    word_in_process[b] = (word_in_process[b] ^ word_in_process[c]).rotate_right(12);
+    word_in_process[a] = (word_in_process[a] + word_in_process[b] + y) % u32::MAX;
+    word_in_process[d] = (word_in_process[d] ^ word_in_process[a]).rotate_right(12);
     word_in_process[c] = (word_in_process[c] + word_in_process[d]) % u32::MAX;
-    word_in_process[b] = (word_in_process[b] ^ word_in_process[c]).rotate_right(7); 
+    word_in_process[b] = (word_in_process[b] ^ word_in_process[c]).rotate_right(7);
 
-
-    let word_in_process =word_in_process;
+    let word_in_process = word_in_process;
     *word_in_process
 }
 
+fn compress(h: [u32; 8], msg: [u32; 16], t: [u32; 2], len: u32, flags: u32) {
+    let mut v: [u32; 16] = [0; 16];
+    for (index, val) in h.iter().enumerate() {
+        v[index] = *val;
+    }
+    for (index, val) in iv.iter().enumerate() {
+        v[index + 8] = *val;
+    }
+    v[12] = t[0];
+    v[13] = t[1];
+    v[14] = len;
+    v[15] = flags;
 
-fn compress(h:[u32;8],msg:[u32;16],t:[u32;2],len:u32,flags:u32){
-    let mut v:[u32;16]=[0;16];
-    v[0..7] = h[0..7];
-    v[8..11] = iv[0..3];
+    for rounds in 0..6 {
+        v = quarter_round_fn(&mut v, 0, 4, 8, 12, msg[0], msg[1]);
+        v = quarter_round_fn(&mut v, 1, 5, 9, 13, msg[2], msg[3]);
+        v = quarter_round_fn(&mut v, 2, 6, 10, 14, msg[4], msg[5]);
+        v = quarter_round_fn(&mut v, 3, 7, 11, 15, msg[6], msg[7]);
+
+        v = quarter_round_fn(&mut v, 0, 5, 10, 15, msg[8], msg[9]);
+        v = quarter_round_fn(&mut v, 1, 6, 11, 12, msg[10], msg[11]);
+        v = quarter_round_fn(&mut v, 2, 7, 8, 13, msg[12], msg[13]);
+        v = quarter_round_fn(&mut v, 3, 4, 9, 14, msg[15], msg[15]);
+    }
+    permute(msg);
 }
 
-
-
-
-
+// fn construct_merkle_tree() {}
 
 #[cfg(test)]
 mod preprocessing_tests {
@@ -123,7 +124,7 @@ mod preprocessing_tests {
     #[test]
     fn check_chunking_general() {
         let input: Vec<u8> = vec![0, 100, 21, 2, 4, 2, 3, 1];
-        let chunked_op = break_into_chunks_at_once(BreakIntoChunksOptions::CopyValue(input));
-        println!("{:?}", chunked_op);
+        // let chunked_op = break_into_chunks_at_once(BreakIntoChunksOptions::CopyValue(input));
+        // println!("{:?}", chunked_op);
     }
 }
