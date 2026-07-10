b972296c24066269ee7dfd21b78f603095b168f5
hksama <kolhatkarh@gmail.com>
2026-07-10 23:29:24 +0530

    changes to blake3 repo for logging

--- BLAKE3/src/portable.rs
+++ BLAKE3/src/portable.rs
@@ -8,10 +8,18 @@ use std::io::Write;
 
 fn log_state(label: &str, state: &[u32; 16]) {
     // Logging disabled to prevent concurrent write corruption
+    println!("[{}] State: [{:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}]",
+        label,
+        state[0], state[1], state[2], state[3],
+        state[4], state[5], state[6], state[7],
+        state[8], state[9], state[10], state[11],
+        state[12], state[13], state[14], state[15]
+    );
 }
 
 fn log_msg(msg: &str) {
     // Logging disabled to prevent concurrent write corruption
+    println!("{}", msg);
 }
 
 #[inline(always)]
